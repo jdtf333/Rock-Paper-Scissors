6902da7dbc042f6d53c174f5dba1d902dca14da5
jdtf333 <ocsicnarfleinadnaj@gmail.com>
2025-06-24 21:16:33 +0800

    Random Array Function Return Rock, Paper, Scissor

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
-console.log("Hello, world")
+/* Player and Computer choose Rock, Paper or Scissors */
+/* Rock wins over Scissor, Paper wins over Rock and Scissor wins over Paper */
+/* The winner gets 1 score, Tie doesn't add score to both, Loser never adds score */
+/* Make a Player wins, Computer wins and It's a tie message pops up */
+function getComputerChoice() {
+    let computerWeapon = ["Rock", "Paper", "Scissors"];
+    let random = computerWeapon[Math.floor(Math.random() * computerWeapon.length)];
+    return random;
+}
+
+console.log(getComputerChoice());
